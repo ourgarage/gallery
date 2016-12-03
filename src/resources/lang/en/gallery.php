0f4702f276366f3bad44c865ff81a12bc6cc1fd4
Anonim_Khb <alvo4444@gmail.com>
2016-12-03 12:42:04 +1000

    In process

--- src/resources/lang/en/gallery.php
+++ src/resources/lang/en/gallery.php
@@ -7,8 +7,30 @@ return [
             'title' => 'Categories',
             'empty' => 'No categories'
         ],
-        'categories-create-update' => [
-            'title' => 'Category create'
+        'categories-create' => [
+            'title' => 'Category create',
+            'form' => [
+                'name' => [
+                    'title' => 'Name',
+                    'placeholder' => 'Enter a category name'
+                ],
+                'description' => [
+                    'title' => 'Description',
+                    'placeholder' => 'Enter a category description'
+                ],
+                'icon' => [
+                    'title' => 'Icon',
+                    'placeholder' => 'Enter a category icon',
+                    'info' => [
+                        'title' => 'How to add a icon?',
+                        'text' => 'To add an icon to select on of the icons <a data="tb" href="http://fontawesome.io/icons/"><strong>Font Awesome</strong></a> collection. <br> You should have something like this: <i><strong>fa fa-address-book</strong></i>'
+                    ]
+                ],
+                'submit' => [
+                    'create' => 'Create',
+                    'update' => 'Update'
+                ]
+            ]
         ],
         'title' => [
             'pictures-index' => 'Pictures gallery',
--- src/resources/views/admin/categories/create.blade.php
+++ src/resources/views/admin/categories/create.blade.php
@@ -1,23 +1,77 @@
 @extends('admin.main')
 
 @section('body-title')
-    {{ trans('gallery::gallery.admin.categories-create-update.title') }}
+    {{ trans('gallery::gallery.admin.categories-create.title') }}
 @endsection
 
 @section('body')
-    <div class="container-fluid">
-        @include('admin.basis.notifications-page')
-        <form role="form" method="POST" action="{{ route('login.post') }}">
-            {!! csrf_field() !!}
-            <div class="row">
-                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 form-group has-feedback">
-                    <input type="email" name="email" class="form-control" placeholder="{{ trans('auth.form.login-email-placeholder') }}"
-                           value="{{ old('email') }}">
-                    <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
+    <section class="content">
+        <div class="container-fluid">
+            @include('admin.basis.notifications-page')
+            {{--<form class="form-horizontal" role="form" method="POST" action="{{ route('login.post') }}">--}}
+            {{--{!! csrf_field() !!}--}}
+            {{--<div class="form-group has-feedback">--}}
+            {{--<label class="col-md-2">Title : *</label>--}}
+            {{--<div class="col-md-8">--}}
+            {{--<input type="text" name="title" class="form-control" value="">--}}
+            {{--<span class="glyphicon glyphicon-header form-control-feedback"></span>--}}
+            {{--</div>--}}
+            {{--</div>--}}
+            {{--<button type="submit" class="btn btn-primary btn-block btn-flat">{{ trans('auth.button.login-submit') }}</button>--}}
+            {{--</form>--}}
+
+            <form class="form-horizontal">
+                <div class="form-group">
+                    <label class="col-sm-2 control-label">
+                        {{ trans('gallery::gallery.admin.categories-create.form.name.title') }}
+                    </label>
+                    <div class="input-group col-sm-9">
+                        <input type="text" name="name"
+                               placeholder="{{ trans('gallery::gallery.admin.categories-create.form.name.placeholder') }}"
+                               class="form-control">
+                        <span class="input-group-addon"><i class="fa fa-tag"></i></span>
+                    </div>
                 </div>
-            </div>
-            <button type="submit" class="btn btn-primary btn-block btn-flat">{{ trans('auth.button.login-submit') }}</button>
-        </form>
-    </div>
+                <div class="form-group">
+                    <label class="col-sm-2 control-label">
+                        {{ trans('gallery::gallery.admin.categories-create.form.description.title') }}
+                    </label>
+                    <div class="input-group col-sm-9">
+                        <input type="text" name="description"
+                               placeholder="{{ trans('gallery::gallery.admin.categories-create.form.description.placeholder') }}"
+                               class="form-control">
+                        <span class="input-group-addon"><i class="fa fa-pencil"></i></span>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label class="col-sm-2 control-label">
+                        {{ trans('gallery::gallery.admin.categories-create.form.icon.title') }}
+                    </label>
+                    <div class="input-group col-sm-9">
+                        <input type="text" name="icon"
+                               placeholder="{{ trans('gallery::gallery.admin.categories-create.form.icon.placeholder') }}"
+                               class="form-control">
+                        <span class="input-group-addon"><i class="fa fa-font-awesome"></i></span>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-2"></div>
+                    <div class="col-sm-9 callout callout-info">
+                        <h4>{{ trans('gallery::gallery.admin.categories-create.form.icon.info.title') }}</h4>
+                        <p>{!! trans('gallery::gallery.admin.categories-create.form.icon.info.text') !!}</p>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-11 no-padding">
+                        <button type="submit" class="btn btn-primary pull-right">
+                            {{ isset($category) ? trans('gallery::gallery.admin.categories-create.form.submit.update') :
+                            trans('gallery::gallery.admin.categories-create.form.submit.create')}}
+                        </button>
+                    </div>
+                </div>
+            </form>
+
+        </div>
+    </section>
 @endsection
 
